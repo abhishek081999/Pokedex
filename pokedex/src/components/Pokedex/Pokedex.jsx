75d948c7b441806a2fef47116d0406862c97a62b
abhishek081999 <abhishekbhoranj@gmail.com>
2023-08-27 13:08:16 +0530

    Search functionality With Debouncing

--- pokedex/src/components/Pokedex/Pokedex.jsx
+++ pokedex/src/components/Pokedex/Pokedex.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import PokemonList from "../PokemonList/PokemonList";
 import Search from "../Search/Search";
 
-import './Pokedex.css'
+// CSS import
+import './Pokedex.css';
+import PokemonDetails from "../PokemonDetails/PokemonDetails";
+
 function Pokedex() {
+
+    const [searchTerm, setSearchterm] = useState('');
 
     return (
         <div className="pokedex-wrapper">
-           <Search/>
-           <PokemonList/>
+            <Search updateSearchTerm={setSearchterm} />
+            { (!searchTerm) ? <PokemonList /> : <PokemonDetails key={searchTerm} pokemonName={searchTerm} />}
         </div>
     )
-} 
+}
 
-export default  Pokedex;
+export default Pokedex;
--- pokedex/src/components/Search/Search.jsx
+++ pokedex/src/components/Search/Search.jsx
@@ -1,15 +1,19 @@
-import './Search.css'
-function Search() {
-    return(
-    <div className='search-wrapper'>
-      <input 
-         id='pokemon-name-search'
-        type="text"
-        placeholder="Pokemon name...."
-      />
+import useDebounce from '../../hooks/useDebounce';
+import './Search.css';
 
-     
-    </div>)
+function Search({updateSearchTerm}) {
+    const debouncedCallback = useDebounce((e) => updateSearchTerm(e.target.value))
+    return (
+        <div className="search-wrapper">
+            <input 
+                id="pokemon-name-search"
+                type="text"
+                placeholder="pokemon name...."
+                onChange={debouncedCallback}
+            />
+
+        </div>
+    );
 }
 
 export default Search;
